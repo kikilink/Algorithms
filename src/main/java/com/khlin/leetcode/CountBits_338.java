912c6fd99cf02fd502c3a3394f526bb7ef7b97f8
kwan <546324943@qq.com>
2020-05-03 23:27:41 +0800

    leetcode 338 比特位计数

---
+++ src/main/java/com/khlin/leetcode/CountBits_338.java
@@ -0,0 +1,21 @@
+package com.khlin.leetcode;
+
+public class CountBits_338 {
+    public int[] countBits(int num) {
+        if (num == 0) {
+            return new int[]{0};
+        }
+        int[] bits = new int[num + 1];
+        bits[0] = 0;
+        for (int i = 1; i <= num; i++) {
+            if (i % 2 == 0) {
+                bits[i] = bits[i / 2];
+            } else {
+                bits[i] = bits[i - 1] + 1;
+            }
+        }
+        return bits;
+    }
+
+
+}
